262aae8deb02149a07f58922dbe44665708d96a6
QuantumGhost <obelisk.reg+git@gmail.com>
2025-09-02 22:03:25 +0800

    fix(api): fix `DetachedInstanceError` for `current_user`
    here we query user with a dedicated session and set
    `expire_on_commit=False` to avoid trigger `DetachedInstanceError`
    while using it after the bounded session has closed.

--- api/services/account_service.py
+++ api/services/account_service.py
@@ -118,33 +118,34 @@ class AccountService:
 
     @staticmethod
     def load_user(user_id: str) -> None | Account:
-        account = db.session.query(Account).filter_by(id=user_id).first()
-        if not account:
-            return None
-
-        if account.status == AccountStatus.BANNED.value:
-            raise Unauthorized("Account is banned.")
-
-        current_tenant = db.session.query(TenantAccountJoin).filter_by(account_id=account.id, current=True).first()
-        if current_tenant:
-            account.set_tenant_id(current_tenant.tenant_id)
-        else:
-            available_ta = (
-                db.session.query(TenantAccountJoin)
-                .filter_by(account_id=account.id)
-                .order_by(TenantAccountJoin.id.asc())
-                .first()
-            )
-            if not available_ta:
+        with Session(db.engine, expire_on_commit=False) as session, session.begin():
+            account = session.query(Account).filter_by(id=user_id).first()
+            if not account:
                 return None
 
-            account.set_tenant_id(available_ta.tenant_id)
-            available_ta.current = True
-            db.session.commit()
+            if account.status == AccountStatus.BANNED.value:
+                raise Unauthorized("Account is banned.")
 
-        if naive_utc_now() - account.last_active_at > timedelta(minutes=10):
-            account.last_active_at = naive_utc_now()
-            db.session.commit()
+            current_tenant = session.query(TenantAccountJoin).filter_by(account_id=account.id, current=True).first()
+            if current_tenant:
+                account.set_tenant_id(current_tenant.tenant_id)
+            else:
+                available_ta = (
+                    session.query(TenantAccountJoin)
+                    .filter_by(account_id=account.id)
+                    .order_by(TenantAccountJoin.id.asc())
+                    .first()
+                )
+                if not available_ta:
+                    return None
+
+                account.set_tenant_id(available_ta.tenant_id)
+                available_ta.current = True
+                session.commit()
+
+            if naive_utc_now() - account.last_active_at > timedelta(minutes=10):
+                account.last_active_at = naive_utc_now()
+                session.commit()
 
         return account
 
